7aedd9bdb4c8248dbe8be0a4617b3ef26a91cc6b
M AHSAN ARSHAD <165573133+Ahsan99r@users.noreply.github.com>
2024-06-05 12:06:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+(async () => {
+    const answer = await inquirer.prompt([
+        {
+            name: "sentence",
+            type: "input",
+            message: "Enter your sentence to count the words: "
+        }
+    ]);
+    const words = answer.sentence.trim().split(" ");
+    console.log(words);
+    console.log(`Your sentence word count is ${words.length}`);
+})();
